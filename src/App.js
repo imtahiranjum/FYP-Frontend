1062370e320b1152766532078488fd1db929be16
Tahir Anjum <78358354+imtahiranjum@users.noreply.github.com>
2023-04-11 22:59:25 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+import React, { Component }  from 'react';
+import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from '/client/src/scenes/dashboard/index.jsx';
+import Layout from '/client/src/scenes/layout/index.jsx';
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider> theme={theme}
+          <CssBaseline />
+          <Routes>
+            <Route element={<layout />}>
+              <Route path='/' element = {<Navigate to="/dashboard" replace />} />
+              <Route path='/dashboard' element = {<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
