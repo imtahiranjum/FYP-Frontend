ac8d26ccf9b658d1b156e5ef363134208ee8b6d9
tahir_anjum <imtahiranjum@gmail.com>
2023-04-18 22:28:33 +0500

    Flexbox and Navbar added

--- src/scenes/layout/index.jsx
+++ src/scenes/layout/index.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Box, useMediaQuery } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Navbar from "components/Navbar"
+import FlexBetween from 'components/FlexBetween';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import profleImage from "assets/profile.jpeg"
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
   )
 }
 
