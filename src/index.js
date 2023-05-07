8f8c6b37ae7443dc83276766ea3bda90d41219c2
Tahir Anjum <78358354+imtahiranjum@users.noreply.github.com>
2023-05-07 23:52:28 +0500

    Middleware added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import './index.css';
 import { configureStore } from '@reduxjs/toolkit';
 import globalReducer from 'state';
 import { Provider } from 'react-redux';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { api } from 'state/api';
+import { getDefaultGridFilterModel } from '@mui/x-data-grid';
+
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer, 
   },
-})
+  middleware: (getDefault)=> getDefault().concat(api.middleware)
+});
+setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
