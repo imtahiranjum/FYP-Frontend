ba957d7825a6a09875413bcafe470d33f7e24e9a
Tahir Anjum <imtahiranjum@gmail.com>
2023-11-11 23:42:08 +0500

    Add new routes for on-sale cattle, cattle list, adding new cattle, adding cattle to sale, staff, and doctors.

--- src/App.js
+++ src/App.js
@@ -7,10 +7,17 @@ import { useMemo } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Dashboard from 'scenes/dashboard/index';
 import Layout from 'scenes/layout/index';
+import AddCattleToSale from "scenes/addCattleToSale/index";
+import Staff from "scenes/staff/index";
+import Doctor from "scenes/doctor/index";
+import AllOnSaleCattle from 'scenes/onsalecattle/index';
+import CattleList from 'scenes/cattleDetails';
+import AddNewCattle from 'scenes/addNewCattle';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(()=> createTheme(themeSettings(mode)), [mode]);
+  
   return (
     <div className="app">
       <BrowserRouter>
@@ -20,6 +27,12 @@ function App() {
             <Route element={<Layout />}>
               <Route path='/' element = {<Navigate to="/dashboard" replace />} />
               <Route path='/dashboard' element = {<Dashboard />} />
+              <Route path="/onsalecattle" element={<AllOnSaleCattle />} />
+              <Route path="/cattlelist" element={<CattleList />} />
+              <Route path="/addnewcattle" element={<AddNewCattle />} />
+              <Route path="/addcattletosale" element={<AddCattleToSale />} />
+              <Route path="/staff" element={<Staff />} />
+              <Route path="/doctors" element={<Doctor />} />
             </Route>
           </Routes>
         </ThemeProvider>
